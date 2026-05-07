258b16ca3ff74dffa11317640f4cbb6df3455ea5
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-05-07 14:11:44 -0500

    Complete fso 4.1 and 4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,39 @@
+const blogRouter = require("express").Router()
+const Blog = require("../models/blog")
+
+blogRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+
+blogRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+
+
+// blogRouter.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
+
+//   blog.save().then((result) => {
+//     response.status(201).json(result)
+//   })
+// })
+
+
+module.exports = blogRouter
